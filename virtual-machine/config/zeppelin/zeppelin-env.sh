78b55b077f208d3ce1fa3dc8e913418ebf153fa9
Dominik Kaisers <dominik@dkaisers.net>
2017-11-22 19:23:49 +0100

    Initial

---
+++ virtual-machine/config/zeppelin/zeppelin-env.sh
@@ -0,0 +1,5 @@
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+export HADOOP_HOME=/home/vagrant/hadoop
+export PATH=$JAVA_HOME/bin:$HADOOP_HOME/bin:$PATH
+export HADOOP_CONF=$HADOOP_HOME/etc/hadoop
+export PYTHON_EGG_CACHE=/home/vagrant/.python-eggs
